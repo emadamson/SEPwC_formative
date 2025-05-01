c72ac10d327ce716cf7b41a1d64ef9584c20fe14
emadamson <emily.adamson@hotmail.co.uk>
2025-05-01 16:24:21 +0100

    Inital Code 4/5 tests passed

--- python/todo.py
+++ python/todo.py
@@ -9,13 +9,43 @@ def add_task(task):
     Input - a task to add to the list
     Return - nothing
     """
+    with open(TASK_FILE, "a", encoding="utf-8") as file:
+        file.write(task + "\n")
 
 def list_tasks():
-    return
+    """Function: list_tasks
+
+    Input - a task to add to the list
+    Return - a string of the tasks on the list
+    """
+
+    output_string = ""
+    if os.path.exists(TASK_FILE):
+        with open(TASK_FILE, "r", encoding="utf-8") as file:
+            tasks = file.readlines()
+            for index, task in enumerate(tasks, start=1):
+                output_string += (f"{index}. {task.strip()}\n")
+    return output_string.strip()
+
 
 
 def remove_task(index):
-    return
+    """Function: remove_task
+
+    Input - a task to add to the list
+    Return - nothing
+    """
+
+    if os.path.exists(TASK_FILE):
+        with open(TASK_FILE, "r", encoding="utf-8") as file:
+            tasks = file.readlines()
+        with open(TASK_FILE, "w", encoding="utf-8") as file:
+            for i, task in enumerate(tasks, start=1):
+                if i != index:
+                    file.write(task)
+        print("Task removed.")
+    else:
+        print("No tasks found.")
 
 def main():
     parser = argparse.ArgumentParser(description="Command-line Todo List")
